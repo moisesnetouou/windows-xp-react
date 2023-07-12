90c4c4ed030d1664bcdd8582d963e8be2d3053dd
Moisés <moisesnetored@gmail.com>
2023-07-12 09:14:19 -0400

    chore: eslint and prettier next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
+import Image from 'next/image';
+import { useEffect } from 'react';
+
 export default function Home() {
   return (
     <div>
       <p>Hello World</p>
+      <Image src="/public/favicon.ico" alt="" />
     </div>
   );
 }
